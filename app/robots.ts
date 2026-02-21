7ccc1578af0aa9422217827feaa4b70529719354
Acharya Group <acharyasumit786@gmail.com>
2026-02-21 14:58:40 +0530

    add sitemap

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://ejaipur.in/sitemap.xml",
+    sitemap: "https://www.ejaipur.in/sitemap.xml",
   };
 }
---
+++ app/sitemap.ts
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://www.ejaipur.in/",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1.0,
+    },
+  ];
+}
